21ca1e908680d4f815f70f82653944d67300cd73
DanialEdil <danial.200316@gmail.com>
2021-01-21 15:01:53 +0600

    добавил нужные файлы

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        num: null,
+    },
+    computed: {
+        countKg: function () {
+            return this.num * 0.453592;
+        },
+        countG: function () {
+            return this.num * 453.592;
+        },
+        countOunc: function () {
+            return this.num * 16;
+        }
+    },
+    methods: {
+        add: function () {
+            this.num++;
+        },
+        minus: function () {
+            this.num--;
+        }
+    }
+});
