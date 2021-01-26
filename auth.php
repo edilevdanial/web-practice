ff563be8fd460864b85196d41ae9e73d7cd4bfe7
Dias <serikov.2002.12@gmail.com>
2021-01-26 13:00:04 +0600

    Вход и регистрация PHP

---
+++ auth.php
@@ -0,0 +1,30 @@
+<?php session_start();
+$connection = mysqli_connect('localhost','root','root','practice');  /*хост атауы, учетная запись, пароль, БД-аты */
+$select_db = mysqli_select_db($connection, 'users'); /*practice - таблица атауы*/
+
+if (isset($_POST["login"]) and isset($_POST["password"])){
+	$login = $_POST["login"];
+	$password = $_POST["password"];
+	#$password = password_hash($password, PASSWORD_DEFAULT);
+	$password = md5($password."ghjsfkld2345");
+	$query = "SELECT * FROM users WHERE login = '$login' and password = '$password'";
+	$rezult = mysqli_query($connection,$query)or die(mysqli_error($connection));
+	$count = mysqli_num_rows($rezult);
+	if ($count == 1){
+		$_SESSION['login'] = $login;
+        $login = $_SESSION['login'];
+        header('Location: /succses.html');
+	}
+	else{
+		$fsmsg = "Ошибка! Попробуйте заново!";
+    }
+}
+
+
+#if(isset($_SESSION['username'])){
+#$username = $_SESSION['username'];
+#echo "hello".$username."";
+#echo "вы вошли";
+#echo "<a href='logal.php' class='btn btn-to btn-primary'>Logout</a>";
+#} 
+ ?>
