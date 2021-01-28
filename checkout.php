7ed1adf89b1ed6f94d7ee80fee89ff0ceffedca6
Dias <serikov.2002.12@gmail.com>
2021-01-28 15:51:51 +0600

    checkout рефакторинг

--- checkout.php
+++ checkout.php
@@ -1,18 +1,16 @@
 <?php
-//$connection  = @mysqli_connect('localhost', 'root', '', 'practice') or die('Ошибка соединения с БД');
-//mysqli_set_charset($practice, "utf8") or die('Не установлена кодировка');
-$connection = mysqli_connect('localhost','root','root','practice');  /*хост атауы, учетная запись, пароль, БД-аты */
-$select_db = mysqli_select_db($connection, 'checkout'); /*practice - таблица атауы*/
-
-if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["number"]) && isset($_POST["theme"])){
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $number = $_POST["number"];
-    $theme = $_POST["theme"];
-
-    $query="INSERT INTO checkout (name, email, number, theme) VALUES ('$name','$email','$number','$theme')";
-    $rezult = mysqli_query($connection, $query);
-    header('Location: /html/success.html');
-}
-
+    $connection = mysqli_connect('localhost','root','root','practice');  /*хост атауы, учетная запись, пароль, БД-аты */
+    $select_db = mysqli_select_db($connection, 'checkout'); /*practice - таблица атауы*/
+    
+    if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["number"]) && isset($_POST["theme"])){
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $number = $_POST["number"];
+        $theme = $_POST["theme"];
+    
+        $query="INSERT INTO checkout (name, email, number, theme) VALUES ('$name','$email','$number','$theme')";
+        $rezult = mysqli_query($connection, $query);
+        header('Location: /html/success.html');
+    }
+    
 ?>
